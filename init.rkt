6f026a5fb75536ec5ab43b0404c730d4b7af1c93
Antigen-1 <zhanghao@antigen.top>
2023-02-15 21:37:48 +0800

    associate the `python-lib` with `python-vm`

--- init.rkt
+++ init.rkt
@@ -1,14 +1,12 @@
 #lang racket/base
 (require data-abstraction)
 
-(define python-lib (get-python-lib))
-(provide python-lib)
-
 (define-data
   python-vm
   ;;https://racket.discourse.group/t/macro-in-racket-language/1709/7?u=antigen-11
   (lib racket/base ffi/unsafe racket/function "lib.rkt")
   (representation
+   (python-lib (get-python-lib))
    (initialize (get-ffi-obj "Py_Initialize" python-lib (_fun -> _void) (thunk (error "initialize:cannot be extracted"))))
    (finalize (get-ffi-obj "Py_FinalizeEx" python-lib (_fun -> (r : _int) -> (if (zero? r) (void) (error "finalize:fail to free all memory allocated by python"))) (thunk (error "finalize:cannot be extracted"))))
    (initialized? (get-ffi-obj "Py_IsInitialized" python-lib (_fun -> (r : _int) -> (if (zero? r) (error "initialized?:the python vm is not initialized successfully") (void))) (thunk (error "initialized?:cannot be extracted")))))

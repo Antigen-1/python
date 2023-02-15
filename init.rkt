4df0eb040c5be6b2790948c0f2c26542f7122a9b
Antigen-1 <zhanghao@antigen.top>
2023-02-15 11:03:40 +0800

    fix

--- init.rkt
+++ init.rkt
@@ -4,9 +4,8 @@
 (define-data
   python-vm
   ;;https://racket.discourse.group/t/macro-in-racket-language/1709/7?u=antigen-11
-  (lib racket/base
-       (ffi/unsafe racket/function "lib.rkt"))
-  (operation
+  (lib racket/base ffi/unsafe racket/function "lib.rkt")
+  (representation
    (initialize (get-ffi-obj "Py_Initialize" python-lib (_fun -> _void) (thunk (error "initialize:cannot be extracted"))))
    (finalize (get-ffi-obj "Py_FinalizeEx" python-lib (_fun -> (r : _int) -> (if (zero? r) (void) (error "finalize:fail to free all memory allocated by python"))) (thunk (error "finalize:cannot be extracted"))))
    (initialized? (get-ffi-obj "Py_IsInitialized" python-lib (_fun -> (r : _int) -> (if (zero? r) (error "initialized?:the python vm is not initialized successfully") (void))) (thunk (error "initialized?:cannot be extracted")))))
@@ -15,5 +14,3 @@
                           (initialize)
                           (initialized?)
                           (dynamic-wind void proc finalize)))))
-(require 'python-vm)
-(provide (all-from-out 'python-vm))

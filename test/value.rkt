b3697b58f285128068b81d0dd8c83e9b95d9ae6c
Antigen-1 <zhanghao@antigen.top>
2023-02-18 22:10:54 +0800

    add value.rkt

---
+++ test/value.rkt
@@ -0,0 +1,15 @@
+#lang racket/base
+(module test racket/base
+  (require racket/function ffi/unsafe "../init.rkt" "../value.rkt")
+  
+  (call-with-python-vm
+   (thunk
+    (let ((v (build-value (list _pointer) "s" #f)))
+      (if (and (none? v)
+               (is? v v))
+          (displayln "successfully build a NONE")
+          (dynamic-wind
+            void
+            (thunk (error "fail to build a NONE"))
+            (thunk (decrement-reference v))))
+      (decrement-reference (call/new-reference v (lambda (v) (build-value (list PyObj*) "(0)" v))))))))

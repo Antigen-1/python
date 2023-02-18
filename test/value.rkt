bd0f2284d3dc0168ac6397813a40c821ad0fef29
Antigen-1 <zhanghao@antigen.top>
2023-02-18 22:39:23 +0800

    update the test

--- test/value.rkt
+++ test/value.rkt
@@ -4,12 +4,18 @@
   
   (call-with-python-vm
    (thunk
-    (let ((v (build-value (list _pointer) "s" #f)))
+    (let* ((v (build-value (list _pointer) "s" #f))
+           (t (call/new-reference v (lambda (v) (build-value (list PyObj*) "(O)" v)))))
       (if (and (none? v)
                (is? v v))
           (displayln "successfully build a NONE")
           (dynamic-wind
             void
             (thunk (error "fail to build a NONE"))
-            (thunk (decrement-reference v))))
-      (decrement-reference (call/new-reference v (lambda (v) (build-value (list PyObj*) "(O)" v))))))))
+            (thunk (decrement-reference t))))
+      (if t
+          (displayln "successfully build the tuple")
+          (dynamic-wind void
+                        (thunk (error "fail to build the tuple"))
+                        (thunk (decrement-reference t))))
+      (decrement-reference t)))))

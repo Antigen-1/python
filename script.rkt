449546753d0307906c47e36f4c7217f5cf63529d
Antigen-1 <zhanghao@antigen.top>
2023-02-19 22:04:35 +0800

    add script.rkt

---
+++ script.rkt
@@ -0,0 +1,25 @@
+#lang racket/base
+(require data-abstraction)
+
+(define-data
+  python-execution
+  (lib ffi-unsafe (only-in "init.rkt" python-lib) (only-in "value.rkt" PyObj*) (only-in "stdio.rkt" FILE*))
+  (representation
+   (run-file (get-ffi-obj 'PyRun_File
+                          python-lib
+                          (_fun FILE*
+                                _file
+                                _int
+                                PyObj*
+                                PyObj* 
+                                -> (r : PyObj*)
+                                -> (if r r (error "fail to run the file")))))
+   (eval-string (get-ffi-obj 'PyRun_String
+                             python-lib
+                             (_fun _string
+                                   _int
+                                   Pyobj*
+                                   PyObj*
+                                   -> (r : _PyObj*)
+                                   -> (if r r (error "fail to eval the string"))))))
+  (abstraction))

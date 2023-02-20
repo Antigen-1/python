ad1c583bb7b87fbc6686f58ff725720b19bf2a7c
Antigen-1 <zhanghao@antigen.top>
2023-02-20 17:56:53 +0800

    update script.rkt

--- script.rkt
+++ script.rkt
@@ -5,6 +5,18 @@
   python-execution
   (lib ffi/unsafe racket/function (only-in "init.rkt" python-lib) (only-in "value.rkt" PyObj*) (only-in "stdio.rkt" FILE*))
   (representation
+   (istolated-expression-symbol (get-ffi-obj 'Py_eval_input
+                                             python-lib
+                                             _int
+                                             (thunk (error "isolated-expression-symbol:cannot be extracted"))))
+   (statement-sequence-symbol (get-ffi-obj 'Py_file_input
+                                           python-lib
+                                           _int
+                                           (thunk (error "statement-sequence-symbol:cannot be extracted"))))
+   (single-statement-symbol (get-ffi-obj 'Py_single_input
+                                         python-lib
+                                         _int
+                                         (thunk (error "single-statement-symbol:cannot be extracted"))))
    (run-file (get-ffi-obj 'PyRun_File
                           python-lib
                           (_fun FILE*

c544c43e954118d08c9bb88d4f5c0bf926bdfd29
Antigen-1 <zhanghao@antigen.top>
2023-02-25 15:58:16 +0800

    a few updates

--- init.rkt
+++ init.rkt
@@ -8,9 +8,9 @@
    (python-lib (get-python-lib))
    (initialize (get-ffi-obj "Py_Initialize" python-lib (_fun -> _void) (thunk (error "initialize:cannot be extracted"))))
    (finalize (get-ffi-obj "Py_FinalizeEx" python-lib (_fun -> (r : _int) -> (if (zero? r) (void) (error "finalize:fail to free all memory allocated by python"))) (thunk (error "finalize:cannot be extracted"))))
-   (initialized? (get-ffi-obj "Py_IsInitialized" python-lib (_fun -> (r : _int) -> (if (zero? r) (error "initialized?:the python vm is not initialized successfully") (void))) (thunk (error "initialized?:cannot be extracted")))))
+   (initialized? (get-ffi-obj "Py_IsInitialized" python-lib (_fun -> _bool) (thunk (error "initialized?:cannot be extracted")))))
   (abstraction
    (call-with-python-vm (lambda (proc)
                           (initialize)
-                          (initialized?)
+                          (if (initialized?) (void) (error "fail to initialize the python vm"))
                           (dynamic-wind void proc finalize)))))

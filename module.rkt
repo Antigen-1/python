553feb66c1dfff14c39658377f73bae69e98388d
Antigen-1 <zhanghao@antigen.top>
2023-02-27 16:47:50 +0800

    fix the procedure

--- module.rkt
+++ module.rkt
@@ -11,11 +11,13 @@
                         (thunk (error "import:cannot be extracted"))))
    (get-object-by-name (lambda (mod obj) (check-and-handle-attribute mod obj get-attribute)))
    (set-python-path (let ((check-and-get (lambda (obj att) (check-and-handle-attribute obj att get-attribute))))
-                      (lazy-load
-                       (let* ((sys-lib (import "sys"))
-                              (path (get-object-by-name sys-lib 'path))
-                              (proc (check-and-get path 'append)))
-                         (at-exit (lambda () (void (map decrement-reference (list path sys-lib)))))
-                         (compose proc (curry build-value (list _path) "s")))
-                       (p)))))
+                      (compose
+                       (lazy-load
+                        (let* ((sys-lib (import "sys"))
+                               (path (get-object-by-name sys-lib 'path))
+                               (proc (check-and-get path 'append)))
+                          (at-exit (lambda () (void (map decrement-reference (list path sys-lib)))))
+                          proc)
+                        (p))
+                       (curry build-value (list _path) "s")))))
   (abstraction))

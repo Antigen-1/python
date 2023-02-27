c38fce3011ad73d4fad3a58d6466719ff222c351
Antigen-1 <zhanghao@antigen.top>
2023-02-27 19:31:28 +0800

    fix the reference counting and add a test

--- lazy.rkt
+++ lazy.rkt
@@ -2,9 +2,11 @@
 (require "init.rkt" "value.rkt" "func.rkt" racket/promise racket/list (for-syntax racket/base))
 (provide lazy-load)
 
+;;以下函数不增加传入对象的引用计数 
 (define build-args (lambda args (let ((l (length args)))
-                                  (apply build-value (make-list l PyObj*) (format "(~a)" (make-string l #\O)) args))))
+                                  (apply build-value (make-list l PyObj*) (format "(~a)" (make-string l #\N)) args))))
 
+;;考虑到python函数对象被封装后不可获得，以下宏会窃取其一个引用计数，请务必保证其引用计数为一
 (define-syntax-rule (lazy-load body (arg ...))
   (let ((promise (delay (let ((proc body))
                           (at-exit (lambda () (void (decrement-reference proc))))
---
+++ test/seq.rkt
@@ -0,0 +1,10 @@
+#lang racket/base
+(require "../init.rkt" "../value.rkt" "../module.rkt" "../seq.rkt" ffi/unsafe)
+
+(call-with-python-vm
+ (lambda ()
+   (define bltn (import "builtins"))
+   (define ls (build-value (list _string PyObj* _int) "[sNi]" "xyz" (build-value (list _string) "(s)" "abc") 100))
+   (define pr (get-onject-by-name bltn 'print))
+   (seq:map (lambda (obj) (call-python-function pr (build-value (list PyObj*) "(N)" obj) #f)) ls)
+   (void (map decrement-reference (list ls pr bltn)))))

89a2da56048c073791d7055cde94a22391daf028
Antigen-1 <zhanghao@antigen.top>
2023-02-27 22:48:39 +0800

    update

--- init.rkt
+++ init.rkt
@@ -10,7 +10,7 @@
    (initialize (get-ffi-obj "Py_Initialize" python-lib (_fun -> _void) (thunk (error "initialize:cannot be extracted"))))
    (finalize 
      (let ((f (get-ffi-obj "Py_FinalizeEx" python-lib (_fun -> (r : _int) -> (if (zero? r) (void) (error "finalize:fail to free all memory allocated by python"))) (thunk (error "finalize:cannot be extracted")))))
-       (thunk (dynamic-wind (thunk (map (lambda (p) (p)) (reverse (unbox cleanup-box))))
+       (thunk (dynamic-wind (thunk (map (lambda (p) (p)) (unbox cleanup-box)))
                             f
                             void))))
    (at-exit (lambda (proc) (set-box! cleanup-box (cons proc (unbox cleanup-box)))))

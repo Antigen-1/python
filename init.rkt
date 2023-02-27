0fe4d979fbff82879e5bdd1fa0e628fb19744561
ZhangHao <zhanghao@antigen.top>
2023-02-27 20:59:42 +0800

    Update init.rkt

--- init.rkt
+++ init.rkt
@@ -8,9 +8,11 @@
    (cleanup-box (box null))
    (python-lib (get-python-lib))
    (initialize (get-ffi-obj "Py_Initialize" python-lib (_fun -> _void) (thunk (error "initialize:cannot be extracted"))))
-   (finalize (thunk (dynamic-wind (thunk (map (lambda (p) (p)) (unbox cleanup-box)))
-                                  (get-ffi-obj "Py_FinalizeEx" python-lib (_fun -> (r : _int) -> (if (zero? r) (void) (error "finalize:fail to free all memory allocated by python"))) (thunk (error "finalize:cannot be extracted")))
-                                  void)))
+   (finalize 
+     (let ((f (get-ffi-obj "Py_FinalizeEx" python-lib (_fun -> (r : _int) -> (if (zero? r) (void) (error "finalize:fail to free all memory allocated by python"))) (thunk (error "finalize:cannot be extracted")))))
+       (thunk (dynamic-wind (thunk (map (lambda (p) (p)) (unbox cleanup-box)))
+                            f
+                            void))))
    (at-exit (lambda (proc) (set-box! cleanup-box (cons proc (unbox cleanup-box)))))
    (initialized? (get-ffi-obj "Py_IsInitialized" python-lib (_fun -> _bool) (thunk (error "initialized?:cannot be extracted")))))
   (abstraction

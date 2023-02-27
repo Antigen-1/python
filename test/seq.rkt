ad990a0eaedd31f83a213d5808d1d69ae0dc15f7
ZhangHao <zhanghao@antigen.top>
2023-02-27 20:46:04 +0800

    fix

--- test/seq.rkt
+++ test/seq.rkt
@@ -6,5 +6,6 @@
    (define bltn (import "builtins"))
    (define ls (build-value (list _string PyObj* _int) "[sNi]" "xyz" (build-value (list _string) "(s)" "abc") 100))
    (define pr (get-object-by-name bltn 'print))
-   (seq:map (lambda (obj) (call-python-function pr (build-value (list PyObj*) "(N)" obj) #f)) ls)
+   (decrement-reference (seq:map (lambda (obj) (call-python-function pr (build-value (list PyObj*) "(N)" obj) #f)) ls))
+   (displayln "done")
    (void (map decrement-reference (list ls pr bltn)))))

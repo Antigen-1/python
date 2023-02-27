17d83d44a7ba97217654b12286605c782c36d7f4
Antigen-1 <zhanghao@antigen.top>
2023-02-27 12:19:46 +0800

    fix the cleanup

--- init.rkt
+++ init.rkt
@@ -5,10 +5,11 @@
   python-vm
   (lib ffi/unsafe racket/function "lib.rkt")
   (representation
+   (cleanup-box (box null))
    (python-lib (get-python-lib))
    (initialize (get-ffi-obj "Py_Initialize" python-lib (_fun -> _void) (thunk (error "initialize:cannot be extracted"))))
    (finalize (get-ffi-obj "Py_FinalizeEx" python-lib (_fun -> (r : _int) -> (if (zero? r) (void) (error "finalize:fail to free all memory allocated by python"))) (thunk (error "finalize:cannot be extracted"))))
-   (at-exit (get-ffi-obj "Py_AtExit" python-lib (_fun (_fun -> _void) -> (r : _int) -> (if (zero? r) (void) (error "fail to cleanup")))))
+   (at-exit (get-ffi-obj "Py_AtExit" python-lib (_fun (_fun #:keep cleanup-box -> _void) -> (r : _int) -> (if (zero? r) (set-box! cleanup-box #f) (error "fail to cleanup")))))
    (initialized? (get-ffi-obj "Py_IsInitialized" python-lib (_fun -> _bool) (thunk (error "initialized?:cannot be extracted")))))
   (abstraction
    (call-with-python-vm (lambda (proc)
--- lazy.rkt
+++ lazy.rkt
@@ -1,8 +1,12 @@
 #lang racket/base
-(require "init.rkt" "value.rkt" racket/promise ffi/unsafe (for-syntax racket/base))
+(require "init.rkt" "value.rkt" "func.rkt" racket/promise (for-syntax racket/base))
 (provide lazy-load)
 
+(define build-args (lambda args (let ((l (length args)))
+                                  (build-value (make-list l PyObj*) (format "(~a)" (make-string l #\0))))))
+
 (define-syntax-rule (lazy-load body (arg ...))
-  (let ((promise (delay body)))
-    (at-exit (lambda () (void (decrement-reference (force promise)))))
-    (lambda (arg ...) ((force promise) arg ...))))
+  (let ((promise (delay (let ((proc body))
+                          (at-exit (lambda () (void (decrement-reference proc))))
+                          proc))))
+    (lambda (arg ...) (call-python-function (force promise) (build-args arg ...) #f))))

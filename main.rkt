3c26dfe0b3800848170cd6b2d2034e457be5e267
Antigen-1 <zhanghao@antigen.top>
2023-02-28 21:26:20 +0800

    update main.rkt

--- main.rkt
+++ main.rkt
@@ -7,11 +7,20 @@
     (provide (all-from-out mod ...))))
 
 (require-and-provide
+ ;;the python VM and the corresponding python shared library
  "init.rkt"
+ ;;set the python shared library name here
  "lib.rkt"
+ ;;constructor, predicates, and reference operators of python objects
  "value.rkt"
+ ;;operators of python objects' attributes
  "object.rkt"
+ ;;python module operators
  "module.rkt"
+ ;;operators and predicate of python functions, including methods of python objects
  "func.rkt"
+ ;;python error handling
  "err.rkt"
+ ;;load python functions from python module
+ "lazy.rkt"
  )

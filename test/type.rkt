a6d5c19ce6c601e029ec67ef13290cdd808b3cc3
Antigen-1 <zhanghao@antigen.top>
2023-03-02 21:34:51 +0800

    fix the test

--- test/type.rkt
+++ test/type.rkt
@@ -4,8 +4,8 @@
 (call-with-python-vm
  (lambda ()
    (define m (import "builtins"))
-   (define t (get-object-by-name m 'object))
-   (define dict (cast (check-and-handle-attribute t '__dict__ get-attribute) PyObj* (pydictof _pyunicode PyObj*)))
+   (define dict (cast (check-and-handle-attribute m '__dict__ get-attribute) PyObj* (pydictof _pyunicode PyObj*)))
+   (decrement-reference m)
    (map (compose decrement-reference cadr) dict)
    (displayln "python `object`'s attributes:")
    (void (map (compose displayln car) dict))))

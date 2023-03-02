b68c5324b3b929bdf3819fd7a882131574d4c928
Antigen-1 <zhanghao@antigen.top>
2023-03-02 21:30:36 +0800

    fix the type

--- test/type.rkt
+++ test/type.rkt
@@ -5,7 +5,7 @@
  (lambda ()
    (define m (import "builtins"))
    (define t (get-object-by-name m 'object))
-   (define dict (cast (check-and-handle-attribute t '__dict__ get-attribute) PyObj* (pydictof (list _pyunicode PyObj*))))
+   (define dict (cast (check-and-handle-attribute t '__dict__ get-attribute) PyObj* (pydictof _pyunicode PyObj*)))
    (map (compose decrement-reference cadr) dict)
    (displayln "python `object`'s attributes:")
    (void (map (compose displayln car) dict))))

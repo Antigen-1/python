92322e7e85ab99555a86a34a9bbc3270f5636414
Antigen-1 <zhanghao@antigen.top>
2023-03-03 21:47:38 +0800

    fix the test

--- test/type.rkt
+++ test/type.rkt
@@ -1,10 +1,10 @@
 #lang racket/base
-(require ffi/unsafe "../object.rkt" "../module.rkt" "../init.rkt" "../type.rkt" "../value.rkt" "../thread.rkt")
+(require ffi/unsafe "../object.rkt" "../module.rkt" "../init.rkt" "../type.rkt" "../value.rkt")
 
 (call-with-python-vm
  (lambda ()
    (define m (import "builtins"))
-   (define dict (run-and-clear (lambda () (cast (check-and-handle-attribute m '__dict__ get-attribute) PyObj* (pydictof _pyunicode PyObj*)))))
+   (define dict (cast (check-and-handle-attribute m '__dict__ get-attribute) PyObj* (pydictof _pyunicode PyObj*)))
    (decrement-reference m)
    (map (compose decrement-reference cadr) dict)
    (displayln "python `object`'s attributes:")

0f2abfd57b26f239fd8f91e86f9c06af24dbbe2b
Antigen-1 <zhanghao@antigen.top>
2023-03-03 21:19:56 +0800

    add thread.rkt

---
+++ thread.rkt
@@ -0,0 +1,24 @@
+#lang racket/base
+(require data-abstraction)
+
+(define-data
+  python-concurrency
+  (lib)
+  (representation
+   (cleanup-box (make-thread-cell #f #f))
+   (add (lambda (v) (let ((b (thread-cell-ref cleanup-box)))
+                      (if b
+                          (set-box! b (cons v (unbox b)))
+                          (thread-cell-set! cleanup-box (box (list v))))
+                      v)))
+   (clear (lambda ()
+            (let ((b (thread-cell-ref cleanup-box)))
+              (if b
+                  (begin (map decrement-reference (unbox b)) (set-box! b null))
+                  (thread-cell-set! cleanup-box (box null)))))))
+  (abstraction
+   (run-and-clear (lambda (proc)
+                    (dynamic-wind
+                      void
+                      proc
+                      clear)))))

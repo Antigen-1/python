f67504a9f4aeb5ee82b12ee1b04e233632bbb142
Antigen-1 <zhanghao@antigen.top>
2023-03-03 21:25:26 +0800

    fix the identifier

--- thread.rkt
+++ thread.rkt
@@ -5,17 +5,17 @@
   python-concurrency
   (lib "value.rkt")
   (representation
-   (cleanup-box (make-thread-cell #f #f))
-   (add (lambda (v) (let ((b (thread-cell-ref cleanup-box)))
+   (object-box (make-thread-cell #f #f))
+   (add (lambda (v) (let ((b (thread-cell-ref object-box)))
                       (if b
                           (set-box! b (cons v (unbox b)))
-                          (thread-cell-set! cleanup-box (box (list v))))
+                          (thread-cell-set! object-box (box (list v))))
                       v)))
    (clear (lambda ()
-            (let ((b (thread-cell-ref cleanup-box)))
+            (let ((b (thread-cell-ref object-box)))
               (if b
                   (begin (map decrement-reference (unbox b)) (set-box! b null))
-                  (thread-cell-set! cleanup-box (box null)))))))
+                  (thread-cell-set! object-box (box null)))))))
   (abstraction
    (run-and-clear (lambda (proc)
                     (dynamic-wind

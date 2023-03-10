f4b90667c260570c7df354de4b2cd4fa6e24503e
Antigen-1 <zhanghao@antigen.top>
2023-03-10 13:57:31 +0800

    add a dependency

--- info.rkt
+++ info.rkt
@@ -1,3 +1,4 @@
 #lang info
 (define collection "simple-python-api")
-(define deps '("git://github.com/Antigen-1/data.git"))
+(define deps '("git://github.com/Antigen-1/data.git"
+               "rash"))
